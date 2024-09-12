3be9258ae24cb8dcddfee3bf4784a64af4ff04bb
Enrique Ulises Báez Gómez Tagle <0241823@up.edu.mx>
2024-09-12 08:29:49 -0600

    feat: Add Caesar cipher decryption for CryptoAnalysis

---
+++ Examen1/Pregunta2/criptoAnalysis-cesar.py
@@ -0,0 +1,29 @@
+def caesar_cipher(message, shift):
+    return ''.join(
+        chr((ord(char) - base + shift) % 26 + base)
+        if char.isalpha() else char
+        for char in message
+        for base in [ord('A') if char.isupper() else ord('a')]
+    )
+
+
+message = """
+Qyyn zsvqbsw, iye ny
+gbyxq iyeb rkxn dyy wemr,
+Grsmr wkxxobvi nofydsyx
+crygc sx drsc;
+Pyb cksxdc rkfo rkxnc
+drkd zsvqbswc’ rkxnc ny
+dyemr,
+Kxn zkvw dy zkvw sc ryvi
+zkvwobc’ uscc.
+"""
+
+# Desplazar con k=-10 al restarle letras, que es lo mismo que sumarle 16 por ser un alfabeto de 26 letras
+shift = 16
+decrypted_text = caesar_cipher(message, shift)
+print("Decrypted text with k={0}:".format(shift), decrypted_text)
+
+shift = -10
+decrypted_text = caesar_cipher(message, shift)
+print("Decrypted text with k={0}:".format(shift), decrypted_text)
