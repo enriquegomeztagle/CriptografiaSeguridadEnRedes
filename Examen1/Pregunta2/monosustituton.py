a74f9a41b06b4bce15d0be2e098a91478344337e
Enrique Ulises Báez Gómez Tagle <0241823@up.edu.mx>
2024-09-11 22:04:36 -0600

    chore: Refactor substitution_cipher function and update variable names

--- Examen1/Pregunta2/monosustituton.py
+++ Examen1/Pregunta2/monosustituton.py
@@ -1,13 +1,13 @@
-def substitution_cipher(message):
+def substitution_cipher(name):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     reversed_alphabet = "zyxwvutsrqponmlkjihgfedcba"
     
     return ''.join(
         reversed_alphabet[alphabet.index(char)] if char.isalpha() else char
-        for char in message.lower()
+        for char in name.lower()
     )
 
 
-original_message = "Enrique Ulises Baez Gomez Tagle"
-encrypted_name = substitution_cipher(original_message)
+name = "Enrique Ulises Baez Gomez Tagle"
+encrypted_name = substitution_cipher(name)
 print(f"Ciphered name: {encrypted_name}")
